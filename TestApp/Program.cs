16f6af03333108569b12ac52044fd8e7e5a6e384
Suren <surendharan.subramaniam@cognizant.com>
2017-08-02 17:26:16 +0530

    merge commit from instance2 into develop

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -13,6 +13,7 @@ namespace TestApp
             Console.WriteLine("merge check change 1");
             Console.WriteLine("merge check change 2");
 
+            Console.WriteLine("merge commit from instance 2 into develop");
         }
     }
 }
